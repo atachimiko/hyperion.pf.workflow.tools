aa8d0d5703dea033f778f74786a6c5eab6bd92ec
atachi <local@example.com>
2018-03-17 22:26:27 +0900

    モデルデータ

---
+++ Xmi2Stm/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xmi2Stm.Model;
+
+namespace Xmi2Stm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new NormalState();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
